a35fe1d1796d4a97f35c07bf09ba59231a8f5647
Edis <Edis@DESKTOP-GU2T6F1>
2019-05-07 10:15:15 -0400

    Primeiro comit da aplicação Segundo comit da aplicação Criar as classes de mapeamento para a hierarquia de pessoas e planos

---
+++ src/java/br/com/academiaif/mapeamento/pessoa/ClienteMapeamento.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.academiaif.mapeamento.pessoa;
+
+import br.com.academiaif.mapeamento.outros.PlanoMapeamento;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Edis
+ */
+public class ClienteMapeamento extends PessoaMapeamento implements Serializable{
+    private PlanoMapeamento planoMapeamento;
+    private int diaVencimento;
+    
+}
